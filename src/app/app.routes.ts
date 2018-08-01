1946f1eb64ecb1b748bddf81132d940f9c379406
estivenm <estivenm930@gmail.com>
2018-07-31 22:59:01 -0500

    agrego componente home, componente navbar y estilos

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+
+const ROUTES_APP: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+
+  //{ path: 'path/:routeParam', component: MyComponent },
+  //{ path: 'staticPath', component: ... },
+  //{ path: '**', component: ... },
+  //{ path: 'oldPath', redirectTo: '/staticPath' },
+  //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(ROUTES_APP)],
+  exports: [RouterModule]
+})
+export class FeatureRoutingModule { }
